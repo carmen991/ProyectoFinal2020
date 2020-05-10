fbfa7e26f827593c227529feaa9c8acbf84e23a9
miranda-monterroso10 <61706522+miranda-monterroso10@users.noreply.github.com>
2020-05-09 18:57:30 -0600

    Update ContactManager.py
    Partes del proyecto

--- ContactManager.py
+++ ContactManager.py
@@ -4,21 +4,10 @@ contactos = {'Miranda Monterroso': '20190405'}
 
 exit = False
 
-def crear_contacto():
+def agregar_contacto():
     input_nom = input("Ingrese nombre del nuevo contacto\n")
     input_tel = input("Ingrese telefono del nuevo contacto\n")
     contactos[input_nom] = input_tel  
-
-def editar_contacto():
-    input_nom = input("Ingrese nombre del contacto que quiere editar\n")
-    input_tel = input("Ingrese nuevo telefono del contacto\n")
-
-    existe = input_nom in contactos
-
-    if existe:
-        contactos[input_nom] = input_tel
-    else:
-        print("El contacto no existe, intentelo de nuevo\n")
 
 def buscar_contacto():
     input_nom = input("Ingrese nombre del contacto que quiere buscar\n")
@@ -26,6 +15,17 @@ def buscar_contacto():
 
     if existe:
         print(input_nom + " " + contactos[input_nom])
+    else:
+        print("El contacto no existe, intentelo de nuevo\n")
+
+def listar_contacto():
+    input_nom = input("Ingrese nombre del contacto que quiere editar\n")
+    input_tel = input("Ingrese nuevo telefono del contacto\n")
+
+    existe = input_nom in contactos
+
+    if existe:
+        contactos[input_nom] = input_tel
     else:
         print("El contacto no existe, intentelo de nuevo\n")
 
@@ -39,31 +39,36 @@ def eliminar_contacto():
     else:
         print("El contacto no existe, intentelo de nuevo\n")
 
-def ver_contactos():
-    for a in contactos:
-        print(a + " " + contactos[a])
+def llamar_contactos():
+    
 
-    for key, value in contactos.items():
-        print(key + " " + value)
+def enviar_mensaje_contactos():
+    
+
+def enviar_correo_contactos():
+    
+
+def exportar_contactos():
+    
 
 while not exit:
 
     input_menu = int(input(" 1. Agregar Contacto \n 2. Buscar Contacto\n 3. Listar Contacto\n 4. Eliminar Contacto\n 5. Llamar Contactos\n 6. Enviar mensaje a contacto\n 7. Enviar correo a contacto\n 8. Exportar Contactos"))
     if input_menu == 1:
-        crear_contacto()
+        agregar_contacto()
     if input_menu == 2:
-        editar_contacto()
-    if input_menu == 3:
         buscar_contacto()
+    if input_menu == 3:
+        listar_contacto()
     if input_menu == 4:
         eliminar_contacto()
     if input_menu == 5:
-        ver_contactos()
+        llamar_contactos()
     if input_menu == 6:
-        ver_contactos()
+        enviar_mensaje_contactos()
     if input_menu == 7:
-        ver_contactos()
+        enviar_correo_contactos()
     if input_menu == 8:
-        ver_contactos()            
+        exportar_contactos()            
     elif input_menu == 9:
         exit = True
